a26caee83b0066679126ebc382d2a3b77ebc9b20
Andrea <andrea.recce@outlook.it>
2023-04-02 19:30:05 +0200

    EX00

---
+++ C00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int	main(int ac, char **av)
+{
+	int i = 1;
+	int k;
+	if (ac == 1)
+	{
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else
+	{
+		while (i < ac)
+		{
+			k = 0;
+			while (av[i][k])
+			{
+				cout << char(toupper(av[i][k]));
+				k++;
+			}
+			i++;
+		}
+	}
+	cout << endl;
+	return (0);
+}
