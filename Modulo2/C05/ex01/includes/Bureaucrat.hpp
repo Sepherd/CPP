2d68629bd68ef2ba3e321978405f97ddbfb09c18
Andrea <andrea.recce@outlook.it>
2023-10-18 22:41:46 +0200

    C05/ex01 added

---
+++ Modulo2/C05/ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sepherd <sepherd@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 16:14:53 by arecce            #+#    #+#             */
+/*   Updated: 2023/10/18 22:23:13 by sepherd          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef Bureaucrat_HPP
+#define Bureaucrat_HPP
+
+#include <string>
+
+class Bureaucrat
+{
+	protected:
+
+		std::string const	_name;
+		int					_grade;
+
+	public:
+
+		Bureaucrat();
+		Bureaucrat(std::string const name, int grade);
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &original);
+		Bureaucrat &operator=(const Bureaucrat &other);
+
+		std::string			getName() const;
+		int					getGrade() const;
+		void				incrementGrade();
+		void				decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+			
+				std::string	what()
+				{
+					return ("1 is the highest possible grade.");
+				}
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+
+				std::string what()
+				{
+					return ("150 is the lowest possibile grade");
+				}	
+		};
+};
+
+std::ostream &operator <<(std::ostream &os, const Bureaucrat &obj);
+
+#endif
