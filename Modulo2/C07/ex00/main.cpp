590fb659e4b8cb2f4536c4ff615830eb1d5d49bd
Andrea <andrea.recce@outlook.it>
2023-11-24 23:06:23 +0100

    C07 finito

--- Modulo2/C07/ex00/main.cpp
+++ Modulo2/C07/ex00/main.cpp
@@ -1,5 +1,11 @@
 #include "includes/Template.hpp"
 
+/*
+I template sono un meccanismo che consente di scrivere codice generico,
+ovvero codice che può funzionare con diversi tipi di dati senza dover essere riscritto per ogni tipo specifico.
+I template vengono utilizzati per creare classi e funzioni generiche.
+*/
+
 int main()
 {
 	int a = 2;
@@ -12,7 +18,7 @@ int main()
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	
+
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
