81f6f08cd11904eded9b1c0b122d63b35a38c00c
Andrea <andrea.recce@outlook.it>
2024-06-18 19:20:36 +0200

    c06 ex01

--- Modulo2/C06/ex01/main.cpp
+++ Modulo2/C06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sepherd <sepherd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 16:14:53 by arecce            #+#    #+#             */
-/*   Updated: 2023/11/24 16:29:41 by sepherd          ###   ########.fr       */
+/*   Updated: 2024/06/18 19:19:21 by sepherd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@ int	main(int ac, char **av)
 	Data data;
 	data.d = 42;
 
+	std::cout << "Data address: " << &data << std::endl;
+
 	uintptr_t	serialized = Serializer::serialize(&data);
 	Data*	deserialized = Serializer::deserialize(serialized);
 
+	std::cout << "Serialized address: " << &serialized << std::endl;
+	std::cout << "Deserialized address: " << deserialized << std::endl;
+	
 	if (deserialized == &data)
 	{
         std::cout << "Serialization and deserialization successful" << std::endl;
