433dd0c7527460f6a756cd63e6232d01e23fb96c
Andrea Recce <arecce@e3r9p8.42roma.it>
2024-06-27 11:11:21 +0200

    ahhhh

--- Modulo2/C07/ex01/main.cpp
+++ Modulo2/C07/ex01/main.cpp
@@ -3,24 +3,27 @@
 int main()
 {
 	int	arr[] = {4, 12, 2, 70, 11, 0};
-	iter(arr, 6, printEl<int>);
-	iter(arr, 6, addTwoEl<int>);
+	size_t	arr_len = sizeof(arr) / sizeof(arr[0]);
+	iter(arr, arr_len, printEl<int>);
+	iter(arr, arr_len, addTwoEl<int>);
 	std::cout << std::endl;
-	iter(arr, 6, printEl<int>);
+	iter(arr, arr_len, printEl<int>);
 	std::cout << std::endl;
 
 	double	arr2[] = {4.2, 12.1, 2.4, 70.7, 11.0, 0.5};
-	iter(arr2, 6, printEl<double>);
+	arr_len = sizeof(arr2) / sizeof(arr2[0]);
+	iter(arr2, arr_len, printEl<double>);
 	std::cout << std::endl;
-	iter(arr2, 6, addTwoEl<double>);
-	iter(arr2, 6, printEl<double>);
+	iter(arr2, arr_len, addTwoEl<double>);
+	iter(arr2, arr_len, printEl<double>);
 	std::cout << std::endl;
 
 	char	arr3[] = {'a', 'n', 'd'};
-	iter(arr3, 3, printEl<char>);
+	arr_len = sizeof(arr3) / sizeof(arr3[0]);
+	iter(arr3, arr_len, printEl<char>);
 	std::cout << std::endl;
-	iter(arr3, 3, addTwoEl<char>);
-	iter(arr3, 3, printEl<char>);
+	iter(arr3, arr_len, addTwoEl<char>);
+	iter(arr3, arr_len, printEl<char>);
 	std::cout << std::endl;
 
 	return 0;
